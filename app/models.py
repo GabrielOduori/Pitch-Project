f3c430e0060034075e526769b51d4e81283d7e33
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-04-23 23:13:09 +0300

    Added classes for Pitch comments on the application

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from datetime import datetime
 from flask_login import UserMixin
 from . import login_manager
-from itsdangerous import TimedJSONWebSignatureSerializer
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 # from email import send_email
 
@@ -16,23 +16,6 @@ Creating a model that will hold all the classes for the key entities
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-
-class Role(db.Model):
-    __tablename__  = 'roles'
-    id  = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(), unique = True)
-    # default = db.Column(db.Boolean, default=False, index=True)
-    # permissions = db.Column(db.Integer)
-    users = db.relationship('User', backref='role', lazy = "dynamic")
-    
-    def __repr__(self):
-        return f'Role {self.name}'
-    
-    
-    def __init__(self, **kwargs):
-        super(Role, self).__init__(**kwargs)
-        if self.permissions is None:
-            self.permissions = 0
 class Permission:
     FOLLOW = 1
     COMMENT = 2
@@ -40,20 +23,75 @@ class Permission:
     MODERATE = 8
     ADMIN = 16
 
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __init__(self, **kwargs):
+        super(Role, self).__init__(**kwargs)
+        if self.permissions is None:
+            self.permissions = 0
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User': [Permission.FOLLOW, Permission.COMMENT, Permission.WRITE],
+            'Moderator': [Permission.FOLLOW, Permission.COMMENT,
+                          Permission.WRITE, Permission.MODERATE],
+            'Administrator': [Permission.FOLLOW, Permission.COMMENT,
+                              Permission.WRITE, Permission.MODERATE,
+                              Permission.ADMIN],
+        }
+        default_role = 'User'
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.reset_permissions()
+            for perm in roles[r]:
+                role.add_permission(perm)
+            role.default = (role.name == default_role)
+            db.session.add(role)
+        db.session.commit()
+
+    def add_permission(self, perm):
+        if not self.has_permission(perm):
+            self.permissions += perm
+
+    def remove_permission(self, perm):
+        if self.has_permission(perm):
+            self.permissions -= perm
+
+    def reset_permissions(self):
+        self.permissions = 0
+
+    def has_permission(self, perm):
+        return self.permissions & perm == perm
+
+    def __repr__(self):
+        return f'Role {self.name}'
+
 class User(UserMixin,db.Model):
     __tablename__  = 'users'
     id = db.Column(db.Integer, primary_key = True)
     email = db.Column(db.String(255), unique = True, index =True)
     username = db.Column(db.String(255))
-    # bio = db.Column(db.String(255))
-    # profile_pic_path = db.Column(db.String())
     password_hash = db.Column(db.String(255))
-    # comment = db.relationship('Comment', backref = 'users', lazy = "dynamic")
-    # pitches  = db.relationship('Pitch', backref = 'users', lazy = "dynamic")
+    # comments = db.relationship('Comment', backref = 'user', lazy = "dynamic")
+    posts = db.relationship('Post', backref = 'user', lazy = "dynamic")
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     confirmed = db.Column(db.Boolean, default = False)
     
-    
+    '''
+    This part of the code is to implement a token system
+    that would be used to send emails to an email account
+    and the user confirms before being added to the list
+    '''
     def generate_confirmation_token(self,expiration=3600):
         s = Serializer(current_app.config['SECRET_KEY'],expiration)
         return s.dumps({'confirm':self.id})
@@ -90,25 +128,26 @@ class User(UserMixin,db.Model):
     def verify_password(self,password):
         return check_password_hash(self.password_hash,password)
     
-class PitchCategory(db.Model):
-    __tablename__='pitch_category'
+class Category(db.Model):
+    __tablename__='categories'
     id  = db.Column(db.Integer, primary_key = True)
     description = db.String(db.String)
-    categories  = db.relationship('PitchCategory', backref = 'pitches', lazy = "dynamic")
+    posts  = db.relationship('Post', backref = 'category', lazy = "dynamic")
     
     def __init__(self,description):
         self.description = description
     
     
-class Pitch(db.Model):
-    __tablename__ = 'pitches'
+class Post(db.Model):
+    __tablename__ = 'posts'
     id  = db.Column(db.Integer, primary_key = True)
     body = db.Column(db.String)
-    timestamp = db.Column(db.DateTime, default = datetime.utcnow)
-    users_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    category  = db.relationship('PitchCategory', backref = 'pitches', lazy = "dynamic")
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    category_id  = db.Column(db.Integer, db.ForeignKey('categories.id'))
     
-#     def __init__(self,pitch_text,user_id):
+    
+#     def __init__(self,post_text,user_id):
 #         self.pitch_text = pitch_text
 #         self.user_id = user_id
         
@@ -124,30 +163,31 @@ class Pitch(db.Model):
 #         return pitches
     
     
-# class Comment(db.Model):
-#     __tablename__='comments'
-#     id  = db.Column(db.Integer, primary_key = True)
-#     comment_text = db.Column(db.String)
-#     comment_date = db.Column(db.DateTime, default = datetime.utcnow)
-#     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-#     pitch_id = db.Column(db.Integer, db.ForeignKey("pitch.id"))
+class Comment(db.Model):
+    __tablename__='comments'
+    id  = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String)
+    timestamp = db.Column(db.DateTime, default = datetime.utcnow)
+    disabled = db.Column(db.Boolean)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    post_id = db.Column(db.Integer, db.ForeignKey("pitch.id"))
     
-#     def __init__(self,comment_date,comment_text,user_id,pitch_id):
-#         self.comment_text = comment_text
-#         self.comment_date = comment_date
-#         self.user_id = user_id
-#         self.pitch_id = pitch_id
+    def __init__(self,comment_date,comment_text,user_id,pitch_id):
+        self.comment_text = comment_text
+        self.comment_date = comment_date
+        self.user_id = user_id
+        self.pitch_id = pitch_id
     
     
-#     def save_comment(self):
-#         db.session.add(self)
-#         db.session.commit()
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
         
         
-#     @classmethod
-#     def get_comments(cls,id):
-#         comments = Comment.query.filer_by(pitch_id = id).all()
-#         return comments
+    @classmethod
+    def get_comments(cls,id):
+        comments = Comment.query.filer_by(pitch_id = id).all()
+        return comments
     
     
     
