41c1acbe30683bb7d36e4a59e20e02839ef91aa5
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-04-25 22:37:45 +0300

    Updated the models.py and included relationships for users and comments

--- app/models.py
+++ app/models.py
@@ -1,11 +1,11 @@
 
-from . import db
+from . import db, login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
 from datetime import datetime
-from flask_login import UserMixin
+from flask_login import UserMixin, AnonymousUserMixin
 from . import login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app
+from flask import current_app, request, url_for
 # from email import send_email
 
 
@@ -16,62 +16,13 @@ Creating a model that will hold all the classes for the key entities
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class Permission:
-    FOLLOW = 1
-    COMMENT = 2
-    WRITE = 4
-    MODERATE = 8
-    ADMIN = 16
-
 
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     default = db.Column(db.Boolean, default=False, index=True)
-    permissions = db.Column(db.Integer)
     users = db.relationship('User', backref='role', lazy='dynamic')
-
-    def __init__(self, **kwargs):
-        super(Role, self).__init__(**kwargs)
-        if self.permissions is None:
-            self.permissions = 0
-
-    @staticmethod
-    def insert_roles():
-        roles = {
-            'User': [Permission.FOLLOW, Permission.COMMENT, Permission.WRITE],
-            'Moderator': [Permission.FOLLOW, Permission.COMMENT,
-                          Permission.WRITE, Permission.MODERATE],
-            'Administrator': [Permission.FOLLOW, Permission.COMMENT,
-                              Permission.WRITE, Permission.MODERATE,
-                              Permission.ADMIN],
-        }
-        default_role = 'User'
-        for r in roles:
-            role = Role.query.filter_by(name=r).first()
-            if role is None:
-                role = Role(name=r)
-            role.reset_permissions()
-            for perm in roles[r]:
-                role.add_permission(perm)
-            role.default = (role.name == default_role)
-            db.session.add(role)
-        db.session.commit()
-
-    def add_permission(self, perm):
-        if not self.has_permission(perm):
-            self.permissions += perm
-
-    def remove_permission(self, perm):
-        if self.has_permission(perm):
-            self.permissions -= perm
-
-    def reset_permissions(self):
-        self.permissions = 0
-
-    def has_permission(self, perm):
-        return self.permissions & perm == perm
 
     def __repr__(self):
         return f'Role {self.name}'
@@ -82,10 +33,10 @@ class User(UserMixin,db.Model):
     email = db.Column(db.String(255), unique = True, index =True)
     username = db.Column(db.String(255))
     password_hash = db.Column(db.String(255))
-    # comments = db.relationship('Comment', backref = 'user', lazy = "dynamic")
-    posts = db.relationship('Post', backref = 'user', lazy = "dynamic")
+    comments = db.relationship('Comment', backref = 'author', lazy = "dynamic")
+    posts = db.relationship('Post', backref = 'author', lazy = "dynamic")
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    confirmed = db.Column(db.Boolean, default = False)
+ 
     
     '''
     This part of the code is to implement a token system
@@ -96,7 +47,7 @@ class User(UserMixin,db.Model):
         s = Serializer(current_app.config['SECRET_KEY'],expiration)
         return s.dumps({'confirm':self.id})
     
-    def confim(self,token):
+    def confirm(self,token):
         s = Serializer(current_app.config['SECRET_KEY'])
         try:
             data = s.loads(token)
@@ -128,39 +79,21 @@ class User(UserMixin,db.Model):
     def verify_password(self,password):
         return check_password_hash(self.password_hash,password)
     
-class Category(db.Model):
-    __tablename__='categories'
-    id  = db.Column(db.Integer, primary_key = True)
-    description = db.String(db.String)
-    posts  = db.relationship('Post', backref = 'category', lazy = "dynamic")
-    
-    def __init__(self,description):
-        self.description = description
-    
     
 class Post(db.Model):
     __tablename__ = 'posts'
     id  = db.Column(db.Integer, primary_key = True)
-    body = db.Column(db.String)
+    body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    category_id  = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    category = db.Column(db.String)
+    comments = db.relationship('Comment', backref='post', lazy='dynamic')
     
     
-#     def __init__(self,post_text,user_id):
-#         self.pitch_text = pitch_text
-#         self.user_id = user_id
-        
 
-#     def save_pitch(self):
-#         db.session.add(self)
-#         db.session.commit()
-        
+
          
-#     @classmethod
-#     def get_pitches(cls,id):
-#         pitches = Pitch.query.filer_by(pitch_id = id).all()
-#         return pitches
+
     
     
 class Comment(db.Model):
@@ -168,27 +101,12 @@ class Comment(db.Model):
     id  = db.Column(db.Integer, primary_key = True)
     body = db.Column(db.String)
     timestamp = db.Column(db.DateTime, default = datetime.utcnow)
-    disabled = db.Column(db.Boolean)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    post_id = db.Column(db.Integer, db.ForeignKey("pitch.id"))
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"))
     
-    def __init__(self,comment_date,comment_text,user_id,pitch_id):
-        self.comment_text = comment_text
-        self.comment_date = comment_date
-        self.user_id = user_id
-        self.pitch_id = pitch_id
-    
-    
-    def save_comment(self):
-        db.session.add(self)
-        db.session.commit()
+
         
-        
-    @classmethod
-    def get_comments(cls,id):
-        comments = Comment.query.filer_by(pitch_id = id).all()
-        return comments
-    
+
     
     
     
