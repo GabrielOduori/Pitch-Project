35a96f39c23f315a3ee54469e6d33b449d550796
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-04-26 09:28:11 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://goduori:postgres@localhost/pitchlist'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://goduori:postgres@localhost/pitchlist'
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     
     
